fcdbfbd12d0ae0447b437f3c64ce0f27f15f7986
Gruenewald, Matthias <gruenewald75@yahoo.de>
2015-08-18 06:39:27 +0200

    switched to android studio

--- Source/Platform/Target/Android/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := all32
-APP_PLATFORM := android-9
-APP_STL := stlport_static
-APP_OPTIM := release
-
---
+++ Source/Platform/Target/Android/mobile/src/main/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_ABI := all32
+APP_PLATFORM := android-9
+APP_STL := stlport_static
+APP_OPTIM := release
